cc0662468b241333d2083b8f16bdb66b11a94e63
NightFyre <80198020+xCENTx@users.noreply.github.com>
2024-07-01 17:25:19 -0400

    adjust bone connections & add new socket names

--- Features/Bones.cs
+++ Features/Bones.cs
@@ -7,72 +7,79 @@ namespace EFT.Trainer.Features;
 
 internal static class Bones
 {
-	public const string Pelvis = "Root_Joint/Base HumanPelvis";
-	public const string Spine1 = Pelvis + "/Base HumanSpine1";
-	public const string Spine2 = Spine1 + "/Base HumanSpine2";
-	public const string Spine3 = Spine2 + "/Base HumanSpine3";
-	public const string Neck = Spine3 + "/Base HumanNeck";
-	public const string Head = Neck + "/Base HumanHead";
-		
-	public const string LThigh1 = Pelvis + "/Base HumanLThigh1";
-	public const string LThigh2 = LThigh1 + "/Base HumanLThigh2";
-	public const string LCalf = LThigh2 + "/Base HumanLCalf";
-	public const string LFoot = LCalf + "/Base HumanLFoot";
-	public const string LToe = LFoot + "/Base HumanLToe";
-	public const string LCollarbone = Spine3 + "/Base HumanRibcage/Base HumanLCollarbone";
-	public const string LForearm1 = LCollarbone + "/Base HumanLUpperarm/Base HumanLForearm1";
-	public const string LForearm2 = LForearm1 + "/Base HumanLForearm2";
-	public const string LForearm3 = LForearm2 + "/Base HumanLForearm3";
-	public const string LPalm = LForearm3 + "/Base HumanLPalm";
-	public const string LDigit11 = LPalm + "/Base HumanLDigit11";
-	public const string LDigit12 = LDigit11 + "/Base HumanLDigit12";
-	public const string LDigit13 = LDigit12 + "/Base HumanLDigit13";
-	public const string LDigit21 = LPalm + "/Base HumanLDigit21";
-	public const string LDigit22 = LDigit21 + "/Base HumanLDigit22";
-	public const string LDigit23 = LDigit22 + "/Base HumanLDigit23";
-	public const string LDigit31 = LPalm + "/Base HumanLDigit31";
-	public const string LDigit32 = LDigit31 + "/Base HumanLDigit32";
-	public const string LDigit33 = LDigit32 + "/Base HumanLDigit33";
-	public const string LDigit41 = LPalm + "/Base HumanLDigit41";
-	public const string LDigit42 = LDigit41 + "/Base HumanLDigit42";
-	public const string LDigit43 = LDigit42 + "/Base HumanLDigit43";
-	public const string LDigit51 = LPalm + "/Base HumanLDigit51";
-	public const string LDigit52 = LDigit51 + "/Base HumanLDigit52";
-	public const string LDigit53 = LDigit52 + "/Base HumanLDigit53";
+        //  CORE
+        public const string Root                = "Root_Joint";
+        public const string Pelvis              = Root          + "/Base HumanPelvis";
+        public const string Spine1              = Pelvis        + "/Base HumanSpine1";
+        public const string Spine2              = Spine1        + "/Base HumanSpine2";
+        public const string Spine3              = Spine2        + "/Base HumanSpine3";
+        public const string Neck                = Spine3        + "/Base HumanNeck";
+        public const string Head                = Neck          + "/Base HumanHead";
+        public const string Ribcage             = Spine3        + "/Base HumanRibcage";
 
-	public const string RThigh1 = Pelvis + "/Base HumanRThigh1";
-	public const string RThigh2 = RThigh1 + "/Base HumanRThigh2";
-	public const string RCalf = RThigh2 + "/Base HumanRCalf";
-	public const string RFoot = RCalf + "/Base HumanRFoot";
-	public const string RToe = RFoot + "/Base HumanRToe";
-	public const string RCollarbone = Spine3 + "/Base HumanRibcage/Base HumanRCollarbone";
-	public const string RForearm1 = RCollarbone + "/Base HumanRUpperarm/Base HumanRForearm1";
-	public const string RForearm2 = RForearm1 + "/Base HumanRForearm2";
-	public const string RForearm3 = RForearm2 + "/Base HumanRForearm3";
-	public const string RPalm = RForearm3 +"/Base HumanRPalm";
-	public const string RDigit11 = RPalm + "/Base HumanRDigit11";
-	public const string RDigit12 = RDigit11 + "/Base HumanRDigit12";
-	public const string RDigit13 = RDigit12 + "/Base HumanRDigit13";
-	public const string RDigit21 = RPalm + "/Base HumanRDigit21";
-	public const string RDigit22 = RDigit21 +"/Base HumanRDigit22";
-	public const string RDigit23 = RDigit22 + "/Base HumanRDigit23";
-	public const string RDigit31 = RPalm + "/Base HumanRDigit31";
-	public const string RDigit32 = RDigit31 + "/Base HumanRDigit32";
-	public const string RDigit33 = RDigit32 + "/Base HumanRDigit33";
-	public const string RDigit41 = RPalm + "/Base HumanRDigit41";
-	public const string RDigit42 = RDigit41 + "/Base HumanRDigit42";
-	public const string RDigit43 = RDigit42 + "/Base HumanRDigit43";
-	public const string RDigit51 = RPalm + "/Base HumanRDigit51";
-	public const string RDigit52 = RDigit51 + "/Base HumanRDigit52";
-	public const string RDigit53 = RDigit52 + "/Base HumanRDigit53";
+        //  LEFT SIDE
+        public const string LThigh1             = Pelvis        + "/Base HumanLThigh1";
+        public const string LThigh2             = LThigh1       + "/Base HumanLThigh2";
+        public const string LCalf               = LThigh2       + "/Base HumanLCalf";
+        public const string LFoot               = LCalf         + "/Base HumanLFoot";
+        public const string LToe                = LFoot         + "/Base HumanLToe";
+        public const string LCollarbone         = Ribcage       + "/Base HumanLCollarbone";
+        public const string LUpperArm           = LCollarbone   + "/Base HumanLUpperarm";   //  SHOULDER
+        public const string LForearm1           = LUpperArm     + "/Base HumanLForearm1";
+        public const string LForearm2           = LForearm1     + "/Base HumanLForearm2";   //  ELBOW
+        public const string LForearm3           = LForearm2     + "/Base HumanLForearm3";
+        public const string LPalm               = LForearm3     + "/Base HumanLPalm";
+        public const string LDigit11            = LPalm         + "/Base HumanLDigit11";
+        public const string LDigit12            = LDigit11      + "/Base HumanLDigit12";
+        public const string LDigit13            = LDigit12      + "/Base HumanLDigit13";
+        public const string LDigit21            = LPalm         + "/Base HumanLDigit21";
+        public const string LDigit22            = LDigit21      + "/Base HumanLDigit22";
+        public const string LDigit23            = LDigit22      + "/Base HumanLDigit23";
+        public const string LDigit31            = LPalm         + "/Base HumanLDigit31";
+        public const string LDigit32            = LDigit31      + "/Base HumanLDigit32";
+        public const string LDigit33            = LDigit32      + "/Base HumanLDigit33";
+        public const string LDigit41            = LPalm         + "/Base HumanLDigit41";
+        public const string LDigit42            = LDigit41      + "/Base HumanLDigit42";
+        public const string LDigit43            = LDigit42      + "/Base HumanLDigit43";
+        public const string LDigit51            = LPalm         + "/Base HumanLDigit51";
+        public const string LDigit52            = LDigit51      + "/Base HumanLDigit52";
+        public const string LDigit53            = LDigit52      + "/Base HumanLDigit53";
+
+        //  RIGHT SIDE
+        public const string RThigh1             = Pelvis        + "/Base HumanRThigh1";
+        public const string RThigh2             = RThigh1       + "/Base HumanRThigh2";
+        public const string RCalf               = RThigh2       + "/Base HumanRCalf";
+        public const string RFoot               = RCalf         + "/Base HumanRFoot";
+        public const string RToe                = RFoot         + "/Base HumanRToe";
+        public const string RCollarbone         = Ribcage       + "/Base HumanRCollarbone";
+        public const string RUpperArm           = RCollarbone   + "/Base HumanRUpperarm";   //  SHOULDER
+        public const string RForearm1           = RUpperArm     + "/Base HumanRForearm1";
+        public const string RForearm2           = RForearm1     + "/Base HumanRForearm2";   //  ELBOW
+        public const string RForearm3           = RForearm2     + "/Base HumanRForearm3";
+        public const string RPalm               = RForearm3     + "/Base HumanRPalm";
+        public const string RDigit11            = RPalm         + "/Base HumanRDigit11";
+        public const string RDigit12            = RDigit11      + "/Base HumanRDigit12";
+        public const string RDigit13            = RDigit12      + "/Base HumanRDigit13";
+        public const string RDigit21            = RPalm         + "/Base HumanRDigit21";
+        public const string RDigit22            = RDigit21      + "/Base HumanRDigit22";
+        public const string RDigit23            = RDigit22      + "/Base HumanRDigit23";
+        public const string RDigit31            = RPalm         + "/Base HumanRDigit31";
+        public const string RDigit32            = RDigit31      + "/Base HumanRDigit32";
+        public const string RDigit33            = RDigit32      + "/Base HumanRDigit33";
+        public const string RDigit41            = RPalm         + "/Base HumanRDigit41";
+        public const string RDigit42            = RDigit41      + "/Base HumanRDigit42";
+        public const string RDigit43            = RDigit42      + "/Base HumanRDigit43";
+        public const string RDigit51            = RPalm         + "/Base HumanRDigit51";
+        public const string RDigit52            = RDigit51      + "/Base HumanRDigit52";
+        public const string RDigit53            = RDigit52      + "/Base HumanRDigit53";
 
 	public static readonly List<string[]> Connections =
 	[
 		[Pelvis, LThigh1], [LThigh1, LThigh2], [LThigh2, LCalf], [LCalf, LFoot], [LFoot, LToe], 
 		[Pelvis, RThigh1], [RThigh1, RThigh2], [RThigh2, RCalf], [RCalf, RFoot], [RFoot, RToe], 
 		[Pelvis, Spine1], [Spine1, Spine2], [Spine2, Spine3], [Spine3, Neck], [Neck, Head], 
-		[Spine3, LCollarbone], [LCollarbone, LForearm1], [LForearm1, LForearm2], [LForearm2, LForearm3], [LForearm3, LPalm], 
-		[Spine3, RCollarbone], [RCollarbone, RForearm1], [RForearm1, RForearm2], [RForearm2, RForearm3], [RForearm3, RPalm] 
+        [Neck, LUpperArm], [LUpperArm, LForearm1], [LForearm1, LForearm2], [LForearm2, LForearm3], [LForearm3, LPalm],
+        [Neck, RUpperArm], [RUpperArm, RForearm1], [RForearm1, RForearm2], [RForearm2, RForearm3], [RForearm3, RPalm]
 	];
 
 	public static readonly List<string[]> FingerConnections =
